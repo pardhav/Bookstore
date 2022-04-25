ea732dea7a6dae5cb1803595140c51cfdaf4771a
Pardhav <pardhavneelati@gmail.com>
2022-04-24 19:42:01 -0500

    Changed Book_ID page to SSR

--- src/pages/books/detail/[book_id].tsx
+++ src/pages/books/detail/[book_id].tsx
@@ -17,86 +17,45 @@ import React from "react";
 import axios from "axios";
 import {
   GET_ISBN_COVER_S,
-  useGlobalContext,
-  BookDetails,
-  Works,
   GET_WORKS_INFO,
   GET_ISBN_INFO,
-  isEmpty,
   addBookToCart,
-  getBookDetails,
-  IOpenLibraryDoc,
 } from "@/modules";
-import { bookDetail } from "modules/firebase/testResponse";
 import { AiFillStar } from "react-icons/ai";
+import { FIREBASE_ADMIN } from "modules/firebase/adminApp";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { useAuth } from "modules/firebase/AuthProvider";
 
-function BooksDetail() {
-  const [fetchResult, setFetchResult] = React.useState({} as BookDetails);
-  const [worksResult, setWorksResult] = React.useState({} as Works);
-  const [firestoreResult, setFirestoreResult] = React.useState(
-    {} as IOpenLibraryDoc
-  );
-  const [apiError, setApiError] = React.useState({} as any);
+function BooksDetail(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  console.log({ props });
+  const { firebase, isbn, works } = props;
   const [isAdding, setAdding] = useBoolean();
   const toast = useToast();
   const router = useRouter();
-  const context = useGlobalContext();
-  const fetchBookDetail = async () => {
-    try {
-      if (isEmpty(router.query.book_id as string)) {
-        context.state.showSpinner();
-        const dataFromFirestore = await getBookDetails(
-          router.query.book_id as string
-        );
-        setFirestoreResult(dataFromFirestore as IOpenLibraryDoc);
-        // setFirestoreResult(bookDetail as any);
-        console.log({ dataFromFirestore });
+  const auth = useAuth();
 
-        const res = await axios({
-          method: "GET",
-          url: GET_ISBN_INFO(router.query.book_id as string),
-        });
-        console.log(res.data);
-        setFetchResult(res.data);
-        if (res.data.works && res.data.works.length > 0) {
-          console.log("Getting Works results");
-          const workIdAtZero = res.data.works[0].key;
-          const workRes = await axios({
-            method: "GET",
-            url: GET_WORKS_INFO(workIdAtZero),
-          });
-          console.log(workRes.data);
-          setWorksResult(workRes.data);
-        }
-        context.state.hideSpinner();
-      }
-    } catch (error) {
-      setApiError(error);
-    }
-  };
   const addToCart = async () => {
     try {
       setAdding.on();
-      if (context.state.user && context.state.user.uid) {
+      if (auth.user) {
         await addBookToCart({
-          title: firestoreResult.title,
+          title: firebase.title,
           isbn: router.query.book_id,
-          price: firestoreResult.price,
+          price: firebase.price,
         });
         toast({
           status: "success",
           description: "Book Added to Cart",
           position: "top",
         });
+      } else {
+        router.push("/login");
       }
       setAdding.off();
     } catch (error) {}
   };
-
-  React.useEffect(() => {
-    console.log("----- USE EFFECT OF BOOK_ID ------");
-    fetchBookDetail();
-  }, [router.asPath]);
   return (
     <Layout>
       <Grid templateColumns="repeat(5, 1fr)" column={2}>
@@ -115,42 +74,42 @@ function BooksDetail() {
           <VStack alignItems="flex-start">
             <HStack>
               <Text color={"gray.600"} fontSize={"md"}>
-                {firestoreResult.average_rating}
+                {firebase.average_rating}
               </Text>
               <span>
                 <AiFillStar size={"20px"} color={"#4A5568"} />
               </span>
               <Text color={"gray.600"} fontSize={"md"}>
-                {`(${firestoreResult.ratings_count})`}
+                {`(${firebase.ratings_count})`}
               </Text>
             </HStack>
 
             <Heading fontFamily={"body"} fontWeight={500} fontSize={"3xl"}>
-              {firestoreResult.title}
+              {firebase.title}
             </Heading>
             <Text
               fontSize={"2xl"}
               fontWeight={500}
-            >{`$${firestoreResult.price}`}</Text>
+            >{`$${firebase.price}`}</Text>
             <Text
               fontSize={"lg"}
               fontWeight={500}
-            >{`First Publish Date: ${firestoreResult.publication_date}`}</Text>
+            >{`First Publish Date: ${firebase.publication_date}`}</Text>
             <Text fontSize={"lg"} fontWeight={500}>
-              {`Number of Pages ${firestoreResult.num_pages}`}
+              {`Number of Pages ${firebase.num_pages}`}
             </Text>
-            <Text>{firestoreResult.publisher}</Text>
-            {/* <Text>{firestoreResult.authors_array.join(", ")}</Text> */}
+            <Text>{firebase.publisher}</Text>
+            <Text>{firebase.authors_array.join(", ")}</Text>
             <Text>
-              {typeof worksResult.description === "object"
-                ? (worksResult.description?.value as string)
-                : worksResult.description}
+              {typeof works.description === "object"
+                ? (works.description?.value as string)
+                : works.description}
             </Text>
             <Button
               isFullWidth
               isLoading={isAdding}
-              onClick={() => {
-                addToCart();
+              onClick={async () => {
+                await addToCart();
               }}
               colorScheme="blue"
             >
@@ -163,3 +122,43 @@ function BooksDetail() {
   );
 }
 export default BooksDetail;
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    const bookId = ctx?.params?.book_id as string;
+    const bookRef = FIREBASE_ADMIN.firestore().collection("Books2").doc(bookId);
+    const bookData = (await bookRef.get()).data();
+    const res = await axios({
+      method: "GET",
+      url: GET_ISBN_INFO(bookId),
+    });
+    const isbnRes = res.data;
+    console.log(isbnRes);
+    let workRes;
+    if (isbnRes && isbnRes.works && isbnRes.works[0] && isbnRes.works[0].key) {
+      workRes = (
+        await axios({
+          method: "GET",
+          url: GET_WORKS_INFO(isbnRes.works[0].key),
+        })
+      ).data;
+    }
+
+    return {
+      props: {
+        firebase: { ...bookData },
+        isbn: { ...isbnRes },
+        works: { ...workRes },
+      },
+    };
+  } catch (err) {
+    console.error({ err });
+    ctx.res.end();
+
+    // `as never` prevents inference issues
+    // with InferGetServerSidePropsType.
+    // The props returned here don't matter because we've
+    // already redirected the user.
+    return { props: {} as never };
+  }
+};
